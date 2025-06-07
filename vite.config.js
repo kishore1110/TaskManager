67c75f6746b7f9a470b8c7280a51f4ae0e5f5222
kishore1110 <ganapathinaicker@gmail.com>
2025-06-07 16:27:57 +0530

    initial working

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// ✅ Add this base setting
 export default defineConfig({
+  base: '/TaskManager/',
   plugins: [react()],
 })
